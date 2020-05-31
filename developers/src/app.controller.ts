f5a2fbaad9f543265f1f9af94992358011a209bf
David Cabrera <david.cabrera@fulltimeforce.com>
2020-05-30 22:08:38 -0500

    basic redis setup

--- developers/src/app.controller.ts
+++ developers/src/app.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload, Ctx, RedisContext } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @MessagePattern('tech-rank')
-  getDeveloperRank(data: string[]): number {
+  getDeveloperRank(@Payload() data: string[], @Ctx() context: RedisContext): number {
+    new Logger('RedisTest').log(context);
     return this.appService.getDeveloperRank(data);
   }
 }
--- developers/src/main.ts
+++ developers/src/main.ts
@@ -4,7 +4,7 @@ import { Logger } from '@nestjs/common';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const serviceOptions: MicroserviceOptions = { transport: Transport.TCP, options: { host: '127.0.0.1', port: 3001 } };
+  const serviceOptions: MicroserviceOptions = { transport: Transport.REDIS, options: { url: 'redis://127.0.0.1:3002' } };
   const app = await NestFactory.createMicroservice(AppModule, serviceOptions);
   const logger = new Logger('Developer Microservice');
   await app.listen(() => logger.log('Initialized'));
